922a602878efad46d4c04520a8682834479f3a33
ivanyaschishin <ivanyaschishin@gmail.com>
2016-11-28 23:40:24 +0200

    hack to support HTML5Routing

--- src/main/java/com/isc/todo/manager/TodoApplicationConfiguration.java
+++ src/main/java/com/isc/todo/manager/TodoApplicationConfiguration.java
@@ -3,9 +3,12 @@ package com.isc.todo.manager;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 @ComponentScan("com.isc.todo.manager")
@@ -16,5 +19,10 @@ public class TodoApplicationConfiguration {
         return new ObjectMapper()
                 .enable(SerializationFeature.INDENT_OUTPUT)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+        return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
     }
 }
